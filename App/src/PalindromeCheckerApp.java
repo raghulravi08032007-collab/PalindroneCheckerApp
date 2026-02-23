17a3d180ddbce61f76e29a8e927987b0beeba3be
aanandhita26 <a4aanandhita@gmail.com>
2026-02-23 09:53:17 +0530

    [Name] Added UC-1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,15 @@
 public class PalindromeCheckerApp {
+
+    // Application constants
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome App Management System");
-        System.out.println("Modify this logic to usecase 2");
+
+        System.out.println("======================================");
+        System.out.println("   WELCOME TO PALINDROME CHECKER APP  ");
+        System.out.println("======================================");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("--------------------------------------");
+        System.out.println("System Ready...");
+        System.out.println("--------------------------------------");
     }
 }
